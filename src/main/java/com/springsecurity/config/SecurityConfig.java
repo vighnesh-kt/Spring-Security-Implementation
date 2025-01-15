6f4c89e45dff6e7c9ba18cd1797da5c94c482da0
Vighnesh Tathare <v42069@yahoo.com>
2025-01-15 18:27:03 +0530

    add

---
+++ src/main/java/com/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain sfc(HttpSecurity sec) throws Exception {
+		return sec.build();
+	}
+}
